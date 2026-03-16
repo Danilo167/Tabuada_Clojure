75f89a539415f09981d3ce62fe8d89c3e30383aa
Schei/εγώ <dscr9669@gmail.com>
2026-03-16 19:48:43 -0300

    Add files via upload

---
+++ TabuadaCentenar/main.clj
@@ -0,0 +1,11 @@
+(defn tabuada [numero sinal]
+  (doseq [i (range 0 100)]
+    (println
+      (cond
+        (= sinal +) (str numero " + " i " = " (+ numero i))
+        (= sinal -) (str numero " - " i " = " (- numero i))
+        (= sinal *) (str numero " * " i " = " (* numero i))
+        (= sinal /) (str numero " / " i " = " (/ numero i))
+      ))))
+
+(tabuada 4 +)
